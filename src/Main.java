150c249303e4f4b2a97a65f3ee431f779e587f22
Aleksandr Kilchevskiy <expiratory@yandex.ru>
2024-04-09 23:09:44 +0300

    some fix

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,10 @@
+import methods.Methods;
+
 public class Main {
-    private static int countEvenNumbersInArray(int[] array) {
-        if (array.length == 0) {
-            return 0;
-        }
-
-        int count = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private static int diffBeetweenMaxAndMin(int[] array) {
-        if (array.length == 0) {
-            return 0;
-        }
-
-        int min = array[0];
-        int max = array[0];
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            } else if (array[i] > max) {
-                max = array[i];
-            }
-        }
-
-        return max - min;
-    }
-
-    private static boolean isTwoNeighborsEqualZero(int[] array) {
-        if (array.length == 0) {
-            return false;
-        }
-
-        boolean bool = false;
-
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] == 0 && array[i+1] == 0) {
-                bool = true;
-            }
-        }
-
-        return bool;
-    }
 
     public static void main(String[] args) {
-        System.out.println(countEvenNumbersInArray(new int[] {1, 3, 5}));
-        System.out.println(diffBeetweenMaxAndMin(new int[] {1, 3, 5}));
-        System.out.println(isTwoNeighborsEqualZero(new int[] {1, 3, 5}));
+        System.out.println(Methods.countEvenNumbersInArray(new int[] {1, 3, 5}));
+        System.out.println(Methods.diffBetweenMaxAndMin(new int[] {1, 3, 5}));
+        System.out.println(Methods.isTwoNeighborsEqualZero(new int[] {1, 3, 5}));
     }
 }
---
+++ src/methods/Methods.java
@@ -0,0 +1,54 @@
+package methods;
+
+public class Methods {
+    public static int countEvenNumbersInArray(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int diffBetweenMaxAndMin(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+
+        int min = array[0];
+        int max = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            } else if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        return max - min;
+    }
+
+    public static boolean isTwoNeighborsEqualZero(int[] array) {
+        if (array.length == 0) {
+            return false;
+        }
+
+        boolean bool = false;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == 0 && array[i+1] == 0) {
+                bool = true;
+            }
+        }
+
+        return bool;
+    }
+}
